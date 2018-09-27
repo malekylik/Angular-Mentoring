03ad2a4cf7b817034203640fbbd38a34b7918644
Maksim Kalinouski <Maksim_Kalinouski@epam.com>
2018-09-27 11:09:13 +0300

    core-module/feat: created core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeaderComponent, BreadcrumbsComponent, FooterComponent]
+})
+export class CoreModule { }
