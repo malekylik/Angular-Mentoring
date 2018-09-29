39666bae6e97c424b17e7d622407eed76df490c5
Maxim Kalinovskiy <malekylik@gmail.com>
2018-09-29 18:46:15 +0300

    task-1-intro/feat: implemented course-item component

--- src/app/course/course-item/course-item.component.ts
+++ src/app/course/course-item/course-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Course } from '../course.model';
 
 @Component({
   selector: 'app-course-item',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseItemComponent implements OnInit {
 
+  @Input() course: Course;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  get curseDuration(): string {
+      const hours: number = Math.floor(this.course.duration / 60);
+      const minutes: number = this.course.duration - hours * 60;
+
+      let duration: string = "";
+
+      if (hours != 0) {
+        duration = `${hours}h ${minutes}min`;
+      } else {
+        duration = `${minutes}min`;
+      }
+      
+      return duration;
+  }
 }
--- src/app/course/courses-list/courses-list.component.ts
+++ src/app/course/courses-list/courses-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { coursesListMock } from '../courses-list-mock';
+import { Course } from '../course.model';
+
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.css']
 })
 export class CoursesListComponent implements OnInit {
+
+  courses: Course[] = coursesListMock;
 
   constructor() { }
 
