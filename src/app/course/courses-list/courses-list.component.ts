7c62c4250d59cd5900b666bf00c0017fea98e6c6
Maxim Kalinovskiy <malekylik@gmail.com>
2018-10-02 09:36:34 +0300

    task-2-components/feat: done task with lifecycle hooks

--- src/app/course/courses-list/courses-list.component.ts
+++ src/app/course/courses-list/courses-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, SimpleChanges } from '@angular/core';
 
 import { coursesListMock } from '../courses-list-mock';
 import { Course } from '../course.model';
@@ -8,13 +8,54 @@ import { Course } from '../course.model';
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.css']
 })
-export class CoursesListComponent implements OnInit {
+export class CoursesListComponent implements 
+OnChanges, 
+OnInit, 
+DoCheck,
+AfterContentInit, 
+AfterContentChecked, 
+AfterViewInit, 
+AfterViewChecked, 
+OnDestroy {
 
-  courses: Course[] = coursesListMock;
+  courses: Course[];
 
-  constructor() { }
+  constructor() {
+    this.courses = [];
+   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("onChanges");
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log("onInit");
+    this.courses = coursesListMock;
+  }
+
+  ngDoCheck() {
+    console.log("onDoCheck");
+  }
+
+  ngAfterContentInit() {
+    console.log("afterContentInit");
+  }
+  
+  ngAfterContentChecked() {
+    console.log("afterContentChecked");
+  }
+  
+  ngAfterViewInit() {
+    console.log("afterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("afterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("onDestroy");
   }
 
   onDeleteCourse(id: string): void {
