ae0a5716750c3995a48bf3b7d8c257d3ce1c5dc9
Maksim Kalinouski <Maksim_Kalinouski@epam.com>
2018-10-03 12:37:46 +0300

    task-3-unit-testing/feat: added tests to header

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HeaderComponent } from './header.component';
 
@@ -21,5 +22,21 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render passed logo text', () => {
+    const logoText = fixture.debugElement.query(By.css('.header_logo-text'));
+    component.logo = "logo-text";
+    fixture.detectChanges();
+
+    expect(logoText.nativeElement.innerText).toBe(component.logo);
+  });
+
+  it('should render passed user login', () => {
+    const logoText = fixture.debugElement.query(By.css('.header_user-login'));
+    component.userLogin = "user-login";
+    fixture.detectChanges();
+
+    expect(logoText.nativeElement.innerText).toBe(component.userLogin);
   });
 });
