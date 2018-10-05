24c4a43f95e001d1f8db1ca1e182175eaced3f5e
Maksim Kalinouski <Maksim_Kalinouski@epam.com>
2018-10-05 10:21:39 +0300

    task-4-directives-and-pipes/feat: added no data message to load button

--- src/app/course/courses-list-with-controls/courses-list-with-controls.component.ts
+++ src/app/course/courses-list-with-controls/courses-list-with-controls.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { coursesListMock } from '../courses-list-mock';
+
 @Component({
   selector: 'app-courses-list-with-controls',
   templateUrl: './courses-list-with-controls.component.html',
   styleUrls: ['./courses-list-with-controls.component.css']
 })
 export class CoursesListWithControlsComponent implements OnInit {
+
+  coursesListLength: number = coursesListMock.length;
 
   constructor() { }
 
