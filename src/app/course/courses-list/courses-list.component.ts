20285343b0cc51f85dcd492ad7108a47785fd43d
Maksim Kalinouski <Maksim_Kalinouski@epam.com>
2018-10-08 09:55:37 +0300

    task-4-directives-and-pipes/refactoring: move CourseOrderByPipe from template to class

--- src/app/course/courses-list/courses-list.component.ts
+++ src/app/course/courses-list/courses-list.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { coursesListMock } from '../courses-list-mock';
 import { Course } from '../course.model';
+import { CourseOrderByPipe } from '../course-order-by.pipe';
 
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.css'],
+  providers: [CourseOrderByPipe]
 })
 export class CoursesListComponent implements OnInit {
 
-  courses: Course[];
+  @Input()
+  set courses(newCourses: Course[]) {
+    if (newCourses != this.prevCourses) {
+      this.prevCourses = newCourses;
 
-  constructor() {
-    this.courses = [];
+      this.transformCourses();
+    }
+  }
+
+  get courses(): Course[] {
+    return this.trasformedCourses;
+  }
+
+  trasformedCourses: Course[];
+
+  private prevCourses: Course[];
+
+  constructor(private courseOrderByPipe : CourseOrderByPipe) {
+    this.prevCourses = [];
+    this.trasformedCourses = [];
    }
 
   ngOnInit() {
@@ -22,5 +40,9 @@ export class CoursesListComponent implements OnInit {
 
   onDeleteCourse(id: string): void {
     console.log(id);
+  }  
+
+  private transformCourses(): void {
+    this.trasformedCourses = this.courseOrderByPipe.transform(this.prevCourses);
   }
 }
