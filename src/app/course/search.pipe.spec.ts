d1d7dccd7a91fb482919689c4e834e31c77ca6e4
Maksim Kalinouski <Maksim_Kalinouski@epam.com>
2018-10-08 11:50:16 +0300

    task-4-directives-and-pipes/feat: implemented test for search pipe

--- src/app/course/search.pipe.spec.ts
+++ src/app/course/search.pipe.spec.ts
@@ -1,8 +1,47 @@
 import { SearchPipe } from './search.pipe';
 
+import { Course } from './course.model';
+
+const coursesListMock: Course[] = [
+  {
+    id: "1",
+    title: "Video Course 1",
+    creationTime: `08.10.2018`,
+    duration: 88,
+    description: 'Sleeper pelican gulper slimy sculpin demoiselle duckbill, "Sacramento splittail mudminnow dwarf gourami Australian lungfish Atlantic trout mrigal." Nurseryfish: mullet jellynose fish: bonytail chub spiny-back zebrafish crevice kelpfish dartfish; Atlantic silverside. Rice eel four-eyed fish roach, "roanoke bass." Manta Ray halfmoon Shingle Fish: northern squawfish jack nase barfish combfish bowfin stoneroller minnow. Kelp perch haddock oarfish weever, prickleback pencilfish yellowtail barracuda. Carpetshark butterflyfish; monkeyface prickleback orangestriped triggerfish elasmobranch giant danio ocean sunfish, longnose dace oarfish?"',
+    topRated: false,
+  },
+  {
+    id: "2",
+    title: "Video Course 2",
+    creationTime: "06.10.2018",
+    duration: 27,
+    description: 'Sleeper pelican gulper slimy sculpin demoiselle duckbill, "Sacramento splittail mudminnow dwarf gourami Australian lungfish Atlantic trout mrigal." Nurseryfish: mullet jellynose fish: bonytail chub spiny-back zebrafish crevice kelpfish dartfish; Atlantic silverside. Rice eel four-eyed fish roach, "roanoke bass." Manta Ray halfmoon Shingle Fish: northern squawfish jack nase barfish combfish bowfin stoneroller minnow. Kelp perch haddock oarfish weever, prickleback pencilfish yellowtail barracuda. Carpetshark butterflyfish; monkeyface prickleback orangestriped triggerfish elasmobranch giant danio ocean sunfish, longnose dace oarfish?"',
+    topRated: true,
+  },
+];
+
 describe('SearchPipe', () => {
+  const pipe = new SearchPipe();
+
   it('create an instance', () => {
     const pipe = new SearchPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should correct search', () => {
+    const searchString = '2';
+    const filtredCourses = [
+      {
+        id: "2",
+        title: "Video Course 2",
+        creationTime: "06.10.2018",
+        duration: 27,
+        description: 'Sleeper pelican gulper slimy sculpin demoiselle duckbill, "Sacramento splittail mudminnow dwarf gourami Australian lungfish Atlantic trout mrigal." Nurseryfish: mullet jellynose fish: bonytail chub spiny-back zebrafish crevice kelpfish dartfish; Atlantic silverside. Rice eel four-eyed fish roach, "roanoke bass." Manta Ray halfmoon Shingle Fish: northern squawfish jack nase barfish combfish bowfin stoneroller minnow. Kelp perch haddock oarfish weever, prickleback pencilfish yellowtail barracuda. Carpetshark butterflyfish; monkeyface prickleback orangestriped triggerfish elasmobranch giant danio ocean sunfish, longnose dace oarfish?"',
+        topRated: true,
+      },
+    ];
+  
+    expect(pipe.transform(filtredCourses, searchString)).toEqual(filtredCourses);
   });
 });
