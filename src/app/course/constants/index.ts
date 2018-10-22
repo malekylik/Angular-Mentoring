12a8cae8544b976c69e519a58fe8a6459e48e30c
Maxim Kalinovskiy <malekylik@gmail.com>
2018-10-22 09:20:26 +0300

    task-5-modules-and-services/refactoring: move time constants from CourseRelevanceDirective to separate folder for constants

---
+++ src/app/course/constants/index.ts
@@ -0,0 +1,5 @@
+export const millisecondsPerSecond: number = 1000; // Используй константы
+export const secondsPerMinute: number = 60; // Используй константы
+export const minutesPerHour: number = 60; // Используй константы
+export const hoursPerDay: number = 24; // Используй константы
+export const millisecondsPerDay: number = hoursPerDay * minutesPerHour * secondsPerMinute * millisecondsPerSecond;
--- src/app/course/course-relevance.directive.ts
+++ src/app/course/course-relevance.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
+import { millisecondsPerDay } from './constants';
 
 @Directive({
   selector: '[appCourseRelevance]'
@@ -6,12 +7,6 @@ import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 export class CourseRelevanceDirective implements OnChanges {
 
   @Input('appCourseRelevance') createdDate: string;
-
-  private readonly millisecondsPerSecond: number = 1000; // Используй константы
-  private readonly secondsPerMinute: number = 60; // Используй константы
-  private readonly minutesPerHour: number = 60; // Используй константы
-  private readonly hoursPerDay: number = 24; // Используй константы
-  private readonly millisecondsPerDay: number = this.hoursPerDay * this.minutesPerHour * this.secondsPerMinute * this.millisecondsPerSecond;
 
   constructor(private el: ElementRef) { }
 
@@ -23,7 +18,7 @@ export class CourseRelevanceDirective implements OnChanges {
     const currentDate: number = new Date().getTime();
     const createdDate: number = new Date(this.createdDate).getTime();
     const differenceDate: number = Math.trunc(
-      (createdDate - currentDate) / (this.millisecondsPerDay)
+      (createdDate - currentDate) / (millisecondsPerDay)
     );
 
     if (differenceDate < 0 && differenceDate >= -14) { // Магическое число
