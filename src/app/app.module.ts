9f97eaf7e2d3d900d41da175b80b61f0a9e74389
Maxim Kalinovskiy <malekylik@gmail.com>
2018-10-28 13:44:44 +0300

    task-5-modules-and-services/feat: created login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module'
 import { CourseModule } from './modules/course/course.module';
+import { LoginModule } from './modules/login/login.module';
 import { AuthorizationService } from './services/authorization/authorization.service';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { AuthorizationService } from './services/authorization/authorization.ser
     BrowserModule,
     CoreModule,
     CourseModule,
+    LoginModule,
   ],
   providers: [
     AuthorizationService,
