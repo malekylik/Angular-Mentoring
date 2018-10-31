3956aa3111f455383b7f0c016f98d30e1f9a81a4
Maxim Kalinovskiy <malekylik@gmail.com>
2018-10-31 12:55:48 +0300

    task-5-modules-and-services/feat: implemented AuthorizationService tests

--- src/app/modules/core/services/authorization/authorization.service.spec.ts
+++ src/app/modules/core/services/authorization/authorization.service.spec.ts
@@ -1,14 +1,72 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthorizationService } from './authorization.service';
+import { authorizationStorageToken } from '../../../../constants/authorization';
+import { User } from '../../../../models/user/user.model';
 
 describe('AuthorizationService', () => {
+  const user: User = {
+    id: '1',
+    firstName: 'firstName',
+    lastName: 'lastName',
+  };
+
+  let service: AuthorizationService;
+
   beforeEach(() => TestBed.configureTestingModule({
     providers: [AuthorizationService]
   }));
 
+  beforeEach(() => {
+    service = TestBed.get(AuthorizationService);
+    localStorage.removeItem(authorizationStorageToken);
+  });
+
+  afterAll(() => {
+    localStorage.removeItem(authorizationStorageToken);
+  });
+
   it('should be created', () => {
     const service: AuthorizationService = TestBed.get(AuthorizationService);
     expect(service).toBeTruthy();
+  });
+
+  it('should login user', () => {
+    service.login(user);
+
+    expect(localStorage.getItem(authorizationStorageToken)).toBeTruthy();
+  });
+
+  it('should logout user', () => {
+    service.login(user);
+    service.logout();
+    
+    expect(localStorage.getItem(authorizationStorageToken)).toBeFalsy();
+  });
+
+  describe('isAuthenticated', () => {
+    it('should return false if user is not authenticated', () => {
+      expect(service.isAuthenticated()).toBeFalsy();
+    });
+
+    it('should return true if user is authenticated', () => {
+      service.login(user);
+
+      expect(service.isAuthenticated()).toBeTruthy();
+    });
+  });
+
+  describe('getUserInfo', () => {
+    it('should return null if user is not authenticated', () => {
+      expect(service.getUserInfo()).toBeNull();
+    });
+
+    it('should return user firstName and lastName as string if user is authenticated', () => {
+      const userInfo: string = `${user.firstName} ${user.lastName}`;
+
+      service.login(user);
+
+      expect(service.getUserInfo()).toEqual(userInfo);
+    });
   });
 });
