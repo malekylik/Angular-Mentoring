22949eeebb165342cc616f2605525f3e4f2f3e0f
Maxim Kalinovskiy <malekylik@gmail.com>
2018-10-31 10:44:41 +0300

    task-5-modules-and-services/feat: added static method to BaseUser for User generation

--- src/app/models/user/base-user.ts
+++ src/app/models/user/base-user.ts
@@ -1,9 +1,20 @@
+import hash from 'object-hash';
+
 import { User } from './user.model';
 
 export class BaseUser implements User {
-    constructor(    
+    constructor(
         public id: string,
         public firstName: string,
         public lastName: string
-        ) {}
+    ) { }
+
+    static generateUser(firstName: string, lastName: string): BaseUser {
+        const courseForHash: Partial<User> = {
+            firstName,
+            lastName,
+        };
+
+        return new BaseUser(hash(courseForHash), firstName, lastName);
+    }
 }
