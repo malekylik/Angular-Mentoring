0cd5d95000357e7b3c25b9d354d947e2b6a9a67f
Maxim Kalinovskiy <malekylik@gmail.com>
2018-11-08 12:42:19 +0300

    task-7-routing/feat: implimented ngModule for date inpute and duration input components

--- src/app/modules/course/components/date-input/date-input.component.ts
+++ src/app/modules/course/components/date-input/date-input.component.ts
@@ -1,25 +1,55 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+const noop = () => { };
 
 @Component({
   selector: 'app-date-input',
   templateUrl: './date-input.component.html',
-  styleUrls: ['./date-input.component.scss']
+  styleUrls: ['./date-input.component.scss'],
+  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => DateInputComponent), multi: true }]
 })
-export class DateInputComponent implements OnInit {
+export class DateInputComponent implements OnInit, ControlValueAccessor {
 
   @Input() name: string = '';
   @Input() placeholder: string = '';
-  @Input() date: string = '';
-  @Output() dateChange = new EventEmitter<string>();
+  @Input('required') isRequired = false;
+
+  private innerValue: string = '';
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  changeDate(date: string): void {
-    this.date = date;
-    this.dateChange.emit(date);
+  get date(): string {
+    return this.innerValue;
+  };
+
+  set date(v: string) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
   }
 
+  onBlur() {
+    this.onTouchedCallback();
+  }
+
+  writeValue(value: string) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: (_: any) => void): void {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
 }
--- src/app/modules/course/components/duration-input/duration-input.component.ts
+++ src/app/modules/course/components/duration-input/duration-input.component.ts
@@ -1,25 +1,55 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+const noop = () => { };
 
 @Component({
   selector: 'app-duration-input',
   templateUrl: './duration-input.component.html',
-  styleUrls: ['./duration-input.component.scss']
+  styleUrls: ['./duration-input.component.scss'],
+  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => DurationInputComponent), multi: true }]
 })
-export class DurationInputComponent implements OnInit {
+export class DurationInputComponent implements OnInit, ControlValueAccessor {
 
   @Input() name: string = '';
   @Input() placeholder: string = '';
-  @Input() duration: number = 0;
-  @Output() durationChange = new EventEmitter<number>();
+  @Input('required') isRequired = false;
+
+  private innerValue: number = 0;
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  changeDuration(duration: number): void {
-    this.duration = duration;
-    this.durationChange.emit(this.duration);
+  get duration(): number {
+    return this.innerValue;
+  };
+
+  set duration(v: number) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
   }
 
+  onBlur() {
+    this.onTouchedCallback();
+  }
+
+  writeValue(value: number) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: (_: any) => void): void {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
 }
