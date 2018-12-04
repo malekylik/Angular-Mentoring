bd230de31e9848a39c6c6fd9a3587a5758a1511a
Maxim Kalinovskiy <malekylik@gmail.com>
2018-12-04 10:05:00 +0300

    task-9-rxjs/feat: implimented LoadingBlockService

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -6,9 +6,11 @@ import { SharedModule } from '../shared/shared.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { Page404Component } from './components/page404/page404.component';
+import { LoadingBlockComponent } from './components/loading-block/loading-block.component';
 import { AuthInterceptor } from './services/auth.interceptor';
 import { AuthorizationService } from './services/authorization/authorization.service';
 import { HttpErrorHandlingService } from './services/http-error-handling/http-error-handling.service';
+import { LoadingBlockService } from './services/loading-block/loading-block.service';
 
 @NgModule({
   imports: [
@@ -19,12 +21,15 @@ import { HttpErrorHandlingService } from './services/http-error-handling/http-er
     HeaderComponent,
     FooterComponent,
     Page404Component,
+    LoadingBlockComponent,
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
     Page404Component,
-  ]
+    LoadingBlockComponent,
+  ],
+  entryComponents: [LoadingBlockComponent],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
@@ -39,6 +44,7 @@ export class CoreModule {
       providers: [
         AuthorizationService,
         HttpErrorHandlingService,
+        LoadingBlockService,
         { provide: HTTP_INTERCEPTORS, multi: true, useClass: AuthInterceptor },
       ]
     }
---
+++ src/app/modules/core/services/loading-block/loading-block.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, ViewContainerRef, ComponentFactoryResolver, ComponentRef, ComponentFactory } from '@angular/core';
+
+import { LoadingBlockComponent } from '../../components/loading-block/loading-block.component';
+
+@Injectable()
+export class LoadingBlockService {
+
+  private rootViewContainer: ViewContainerRef;
+  private loadingBlock: ComponentRef<LoadingBlockComponent>;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+
+  setRootViewContainerRef(viewContainerRef: ViewContainerRef): void {
+    this.rootViewContainer = viewContainerRef;
+  }
+
+  showLoadingBlock(condition: boolean): void {
+    if (condition) {
+      if (!this.loadingBlock) {
+        const factory: ComponentFactory<LoadingBlockComponent> = this.componentFactoryResolver
+          .resolveComponentFactory(LoadingBlockComponent);
+        this.loadingBlock = factory
+          .create(this.rootViewContainer.parentInjector)
+        this.rootViewContainer.insert(this.loadingBlock.hostView);
+        this.rootViewContainer.element.nativeElement.style.overflow = 'hidden';
+      }
+    } else {
+      if (this.loadingBlock) {
+        this.loadingBlock.destroy();
+        this.loadingBlock = null;
+        this.rootViewContainer.element.nativeElement.style.overflow = 'auto';
+      }
+    }
+  }
+}
