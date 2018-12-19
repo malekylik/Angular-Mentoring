c0abca28244bb342dcdd2ea11424ae4f9c6f53a7
Maxim Kalinovskiy <malekylik@gmail.com>
2018-12-19 10:15:21 +0300

    task-10-ngrx/fix: tests for courses-list-with-controls component

--- src/app/modules/course/components/courses-list-with-controls/courses-list-with-controls.component.spec.ts
+++ src/app/modules/course/components/courses-list-with-controls/courses-list-with-controls.component.spec.ts
@@ -5,11 +5,18 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { CoursesListWithControlsComponent } from './courses-list-with-controls.component';
 import { CoursesService } from '../../services/courses.service';
 import { HttpErrorHandlingService } from '../../../core/services/http-error-handling/http-error-handling.service';
 import { LoadingBlockService } from '../../../core/services/loading-block/loading-block.service';
+import { mainReducer } from 'src/app/store/reducers';
+import { AuthEffects } from 'src/app/store/effects/auth.effects';
+import { UserEffects } from 'src/app/store/effects/user.effects';
+import { CoursesEffects } from 'src/app/store/effects/courses.effects';
+import { AuthorizationService } from 'src/app/modules/core/services/authorization/authorization.service';
 
 describe('CoursesListWithControlsComponent', () => {
   let component: CoursesListWithControlsComponent;
@@ -17,8 +24,15 @@ describe('CoursesListWithControlsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatDialogModule, RouterTestingModule, HttpClientTestingModule, MatSnackBarModule],
-      providers: [CoursesService, HttpErrorHandlingService, LoadingBlockService],
+      imports: [
+        MatDialogModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatSnackBarModule,
+        StoreModule.forRoot(mainReducer),
+        EffectsModule.forRoot([AuthEffects, UserEffects, CoursesEffects]),
+      ],
+      providers: [AuthorizationService, CoursesService, HttpErrorHandlingService, LoadingBlockService],
       declarations: [CoursesListWithControlsComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
