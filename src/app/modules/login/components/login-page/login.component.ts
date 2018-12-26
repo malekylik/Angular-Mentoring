8475a1d60ec66a21bcfa013f6b7cb1f65954b0ba
Maxim Kalinovskiy <malekylik@gmail.com>
2018-12-26 12:30:47 +0300

    task-11-forms/refactoring: rework login page to have form controls

--- src/app/modules/login/components/login-page/login.component.ts
+++ src/app/modules/login/components/login-page/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { ValidationService } from 'src/app/modules/core/services/validation/validation.service';
 
 import { BaseUser } from '../../../../models/user/base-user';
 import { State } from 'src/app/models/state.model';
@@ -12,17 +14,36 @@ import { Login } from 'src/app/store/actions/auth.actions';
   styleUrls: ['./login.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-  userLogin: string = "";
-  password: string = "";
 
-  constructor(private store: Store<State>) { }
+  form: FormGroup;
+
+  constructor(
+    private store: Store<State>,
+    private fb: FormBuilder,
+    public validationService: ValidationService,
+    ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      login: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
   }
 
-  login(isValid: boolean): void {
-    if (isValid) {
-      this.store.dispatch(new Login(BaseUser.generateUser('', '', this.userLogin, this.password, '')));
+  get loginControl(): AbstractControl {
+    return this.form.get('login');
+  }
+
+  get passwordControl(): AbstractControl {
+    return this.form.get('password');
+  }
+
+  login(): void {
+    if (this.form.valid) {
+      const login: string = this.loginControl.value;
+      const password: string = this.passwordControl.value;
+
+      this.store.dispatch(new Login(BaseUser.generateUser('', '', login, password, '')));
     }
   }
 }
